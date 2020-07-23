81707e8f4caa6f3559f0852a76538c9bcdc07500
Kate Skorija <kateskorija@gmail.com>
2020-07-23 10:54:10 -0700

    create album and artist models
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>
    Co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ MusicOrganizer/Models/Album.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Models
+{
+
+  public class Album
+  {
+    public string Title { get; set; }
+    public int Id { get; }
+
+    private static List<Album> _instances = new List<Album> { };
+
+    public Album(string title)
+    {
+      Title = title;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Album> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Album Find(int searchId)
+    {
+      return _instances[searchId -1];
+    }
+
+  }
+}
---
+++ MusicOrganizer/Models/Artist.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Models
+{
+    public class Artist
+    {
+    private static List<Artist> _instances = new List<Artist> { };
+    public string Name { get; set; }
+
+    public int Id { get; }
+    public List<Album> Albums { get; set;}
+
+    public Artist(string artistName)
+    {
+      Name = artistName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Albums = new List<Album> { };
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Artist> GetAll()
+    {
+      return _instances;
+    }
+    public static Artist Find(int searchId)
+    {
+      return _instances[searchId -1];
+    }
+
+    public void AddAlbum(Album album)
+    {
+      Albums.Add(album);
+    }
+  } 
+}
